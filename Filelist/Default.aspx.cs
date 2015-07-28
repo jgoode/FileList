c4c8a598c0e88da10b4d5b7192f6bddd1aa4217f
jgoode <johnhgoode@gmail.com>
2015-07-28 06:23:33 -0400

    Get list of files iteration 1

--- Filelist/Default.aspx.cs
+++ Filelist/Default.aspx.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Filelist
-{
-    public partial class _Default : Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+namespace Filelist {
+    public partial class _Default : Page {
+        private List<FileInfo> _files;
+        private const string _directoryName = "//data";
 
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        private void GetFiles() {
+            var directory = new DirectoryInfo(Server.MapPath(".") + _directoryName);
+            _files = directory.GetFiles().ToList();
         }
     }
 }
